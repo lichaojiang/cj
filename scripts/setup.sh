616cbb31768f819353c6c0db3db65d4799e26ac4
Yiling Fan <yiling.fan@bivrost.cn>
2018-11-27 21:13:19 +0800

    add npm install to setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,9 @@ cwd=$(pwd)
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$(cd "${SCRIPT_DIR}/.." && pwd)
 
+# install npm packages
+npm install
+
 conda_link=""
 conda_sh=""
 
