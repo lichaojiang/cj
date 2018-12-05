a7afd5f7ea5b79c30e97282ea439c03f14ca9b8e
Yiling Fan <yiling.fan@bivrost.cn>
2018-12-05 11:40:12 +0800

    modify remote script

--- scripts/remote.sh
+++ scripts/remote.sh
@@ -2,17 +2,17 @@
 if [ "$1" == "start" ];then
     echo -n "Remote server user name:"
     read user
-    ssh "${user}@admin.bivrost.cn" "cd /var/bivServer/ && git reset origin master && npm install && npm run start"
+    ssh "${user}@admin.bivrost.cn" "cd /var/bivServer/ && git reset --hard origin/master && npm install && npm run start"
 elif [ "$1" == "stop" ];then
     echo -n "Remote server user name:"
     read user
-    ssh "${user}@admin.bivrost.cn" "cd /var/bivServer/ && git reset origin master && npm install && npm run stop"
+    ssh "${user}@admin.bivrost.cn" "cd /var/bivServer/ && git reset --hard origin/master && npm install && npm run stop"
 elif [ "$1" == "restart" ];then
     echo -n "Remote server user name:"
     read user
-    ssh "${user}@admin.bivrost.cn" "cd /var/bivServer/ && git reset origin master && npm install && npm run restart"
+    ssh "${user}@admin.bivrost.cn" "cd /var/bivServer/ && git reset --hard origin/master && npm install && npm run restart"
 elif [ "$1" == "setup" ];then
     echo -n "Remote server user name:"
     read user
-    ssh "${user}@admin.bivrost.cn" "cd /var/bivServer/ && git reset origin master && npm install && npm run setup"
+    ssh "${user}@admin.bivrost.cn" "cd /var/bivServer/ && git reset --hard origin/master && npm install && npm run setup"
 fi
