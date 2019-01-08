b5e5d0deaf377a10cc425dbb91871b759aa8e612
Yiling Fan <yiling.fan@bivrost.cn>
2019-01-08 16:44:29 +0800

    chartdata create

--- routes/plot.js
+++ routes/plot.js
@@ -108,7 +108,9 @@ router.post('/', cors(bconst.corsOptions), auth(), function(req, res, next) {
             
             api_plot = path.join(bconst.exedir, "magicbag.py");
             // python magicbag.py "throughput,elasped,setup,poweroff" "throughput/(elasped-setup-poweroff)" "1" "2018-7-16" 7 "08:00:00-12:00:00-1,13:30:00-17:30:00-2"
-            cmdstr = bconst.statspython+" "+api_plot+" "+query.variables+" "+query.recipe+" "+query.machine+" "+
+            let variables = `'${query.variables}'`;
+            let recipe = `'${query.recipe}'`;
+            cmdstr = bconst.statspython+" "+api_plot+" "+variables+" "+recipe+" "+query.machine+" "+
                 query.start_date+" "+query.days+" "+query.intervals;
             console.log("cmd string is:" + cmdstr);
 
