a09b3e3dfb4f56410d98d4d318f25960e2475834
Yiling Fan <yiling.fan@bivrost.cn>
2019-01-16 15:31:00 +0800

    bug fix

--- routes/datatable.js
+++ routes/datatable.js
@@ -2,13 +2,13 @@
 
 var express = require('express');
 var router = express.Router();
-var exec = require('child_process').exec; 
 var url = require('url');
 const path = require('path');
 const cors = require('cors');
 const butil = require('../lib/bUtils');
 const bconst = require('../lib/bConstants');
 const auth = require('../lib/bUtils').userAuth;
+const bres = require('../lib/bResponse');
 
 /* global var*/
 //var exedir=(os.type()=="Windows_NT")?
@@ -25,13 +25,27 @@ router.get('/', cors(bconst.corsOptions), auth(), function(req, res, next) {
             api_bivstats = path.join(bconst.exedir, "stats.py");
             cmdstr = bconst.statspython+" "+api_bivstats+" "+params.start+" "+params.end+" "+params.type+" "+params.machine;
             console.log("cmd string is:"+cmdstr);
-            butil.execute(res, cmdstr, butil.parseStdout);
+            butil.execute(cmdstr, butil.parseStdout).then(output => {
+                bres.send(res, output.data, output.status);
+            }).catch(err =>{
+                let err_status = bres.findStatus(err);
+                console.log(err_status);
+                console.log(err.stack);
+                bres.send(res, null, err_status);
+            });;
             break;
         case "getDataByTime":     
             api_get_data = path.join(bconst.exedir, "getDataByTime.py");
             cmdstr = bconst.statspython+" "+api_get_data+" "+params.start+" "+params.end+" "+params.type+" "+params.machine;					
             console.log("cmd string is:"+cmdstr);
-            butil.execute(res, cmdstr, butil.parseStdout);                                       	    
+            butil.execute(cmdstr, butil.parseStdout).then(output => {
+                bres.send(res, output.data, output.status);
+            }).catch(err =>{
+                let err_status = bres.findStatus(err);
+                console.log(err_status);
+                console.log(err.stack);
+                bres.send(res, null, err_status);
+            });;                                       	    
 			break;
         default:
 			break;
